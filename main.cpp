35e5609f9ce4b56ac0c8f0970440331ffac9459e
badzu <badzu@users.noreply.github.com>
2017-11-17 19:06:22 +0100

    0.2.1
    Dodano
    -Bardziej rozbudowany Pokoj 1
    -Pojawily sie instrukcje warunkowe if oraz petla goto

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@
 #include<math.h>
 #include<cstdlib>
 #include<stdio.h>
+#include<fstream>
 using namespace std;
 
-	int x, y;
+	int key;
 	char wybor;
 	
 
 	int main()
 
-	{
-		
+	{	
 		cout << "  |              __  __   _   _   ______    _____   _____   _    _   __  __   |" << endl;
 		cout << "  |      /\\     |  \\/  | | \\ | | |  ____|  / ____| |_   _| | |  | | |  \\/  |  |" << endl;
 		cout << "  |     /  \\    | \\  / | |  \\| | | |__    | (___     | |   | |  | | | \\  / |  |" << endl;
@@ -25,7 +25,7 @@ using namespace std;
 		cout << "    \\             Bartlomiej Ciolkosz All Rights Reserverd 2017.            /"<<endl;
 		cout << "     \\_____________________________________________________________________/"<<endl<<endl<<endl;
 		cout << "                     |   Nacisnij S, aby zaczac gre   |"<<endl;
-		cout << "                     |   Nacisnij Q, aby wyjsc z gry  |"<<endl;
+		cout << "                     |   Nacisnij Q, aby wyjsc z gry  |"<<endl<<endl;
 		
 		
 		wybor = getch();
@@ -40,19 +40,17 @@ using namespace std;
 		    default: cout<< "nie ma takiej opcji";
 		}
 			
-		cout << endl << "Budzisz sie w zamknietym pomieszczeniu na lozku, twoja glowa bardzo cie boli, a obraz poczatkowo wydaje"
+		cout << "Budzisz sie w zamknietym pomieszczeniu na lozku, twoja glowa bardzo cie boli, a obraz poczatkowo wydaje"
 			"sie byc jakby przez mgle. Chwila lezenia pomaga w koncu dojsc do siebie. Pokoj nie jest duzy dostrzegasz jedynie "
-			"biurko, szafe, obraz oraz drzwi" << endl << endl;
+			"biurko, szafe, obraz oraz drzwi";
+			
+		P1:	
+		cout<<endl<<endl;	
 		cout << "Drzwi <1>" << endl;
 		cout << "Stary Obraz <2>" << endl;
 		cout << "Biurko <3>" << endl;
-		cout << "Szafa <4>" << endl;
-		cout << "Wyjscie <Q>" << endl;
-		
-		for(;;)
-		
-		{
-		
+		cout << "Szafa <4>" <<endl;
+				
 				cout<<endl<<endl;
 				cout << "Wybierz rzecz do ktorej chcesz podejsc :"<< endl << endl;
 
@@ -61,24 +59,142 @@ using namespace std;
 		switch (wybor)
 		{
 			case '1':
-				cout << "> Podchodzisz do drzwi";
+				cout << "> Podchodzisz do drzwi"; // PRZYPADEK DRZWI//
+				
+				DR:
+				
+				cout<<endl<<endl;
+				
+				wybor = getch();
+				switch (wybor)			
+		{	
+							
+				case 'a':
+					
+				cout << " > Bardzo stare drzwi wydaja sie byc zamkniete";
+				goto DR;
+				break;
+				
+				case 's':
+					if (key==1)
+				{
+				cout << " > Uzyto klucza drzwi sie otwieraja";
+				} else
+				cout<< " > Nie mozna otworzyc drzwi";
+				goto DR;
+				break;
+				
+				case 'd':
+				cout << " > Nie ma tutaj nic do zebrania";
+				goto DR;
+				break;
+				
+				case 'w':
+				cout << "> Odchodzisz od przedmiotu";
+				goto P1;
+				break;	
+					
+		}		
+				
 			break;
 				case '2':
-					cout << "> Podchodzisz do starego obrazu";
+					cout << "> Podchodzisz do starego obrazu"; // PRZYPADEK OBRAZU //
+					
+					OB:
+					
+							wybor = getch();
+				switch (wybor)
+		{
+				case 'a':
+					
+				cout << " Bardzo stare drzwi wydaja sie byc zamkniete";
+				goto OB;
+				break;
+				
+				case 's':
+					if (key==1)
+				{
+				cout << " > Uzyto klucza drzwi sie otwieraja";
+				} else
+				cout<< " > Nie mozna otworzyc drzwi";
+				goto OB;
+				break;
+				
+				case 'd':
+				cout << " > Nie ma tutaj nic do zebrania";
+				goto OB;
+				break;
+				
+				case 'w':
+				cout << "> Odchodzisz od przedmiotu";
+				goto P1;
+				break;		
+		}
 			break;	
 					case '3':
-						cout << "> Podchodzisz do biurka";
+						cout << "> Podchodzisz do biurka"; // PRZYPADEK BIURKA //
+						
+				BI:	
+						
+								wybor = getch();
+				switch (wybor)
+		{
+				case 'a':
+					
+				cout << " > Przeszukujac biurko odnajdujesz mnostwo kurzu oraz klucz";
+				goto BI;
+				break;
+				
+				case 's':
+								
+				cout<< " > Przedmiotu biurko nie da sie uzyc";
+				goto BI;
+				break;
+				
+				case 'd':
+				cout << " > Wzieto srebrny klucz";
+				key = 1;
+				goto BI;
+				break;
+				
+				case 'w':
+				cout << "> Odchodzisz od przedmiotu";
+				goto P1;
+				break;		
+		}
 			break;
 						case '4':
-							cout << "> Podchodzisz do szafy";
-			break;
-						case 'q':
-							cout << "> Wychodzisz z gry";
-							exit(0);
+							cout << "> Podchodzisz do szafy"; // PRZYPADEK SZAFY //
+							
+									wybor = getch();
+				switch (wybor)
+		{
+				case 'a':
+					
+				cout << " Bardzo stare drzwi wydaja sie byc zamkniete";
+				break;
+				
+				case 's':
+					if (key==1)
+				{
+				cout << " > Uzyto klucza drzwi sie otwieraja";
+				} else
+				cout<< " > Nie mozna otworzyc drzwi";
+				break;
+				
+				case 'd':
+				cout << " > Nie ma tutaj nic do zebrania";
+				break;
+				
+				case 'w':
+				cout << "> Odchodzisz od przedmiotu";
+				goto P1;
+				break;		
+		}
 			break;
 		    default: cout<< "nie ma takiej opcji";
 		}
-}
+
 
 		return (0);
-}
+	}
