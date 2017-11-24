121224961294cb7ed0a7ce2a227ec54c46897919
badzu <badzu@users.noreply.github.com>
2017-11-24 22:45:01 +0100

    0.4.1
    Level eksperymentalny łączy pętle goto i for. #Bez ekranu startowego

--- main.cpp
+++ main.cpp
@@ -4,215 +4,185 @@
 #include<math.h>
 #include<cstdlib>
 #include<stdio.h>
-#include<fstream>
+#include <stdlib.h>
 using namespace std;
 
-	int key;
+	int quit=0, skey, rkey, oknf;
 	char wybor;
 	
 
-	int main()
-
+	int main(){
+	for(;;)
 	{	
-		cout << "  |              __  __   _   _   ______    _____   _____   _    _   __  __   |" << endl;
-		cout << "  |      /\\     |  \\/  | | \\ | | |  ____|  / ____| |_   _| | |  | | |  \\/  |  |" << endl;
-		cout << "  |     /  \\    | \\  / | |  \\| | | |__    | (___     | |   | |  | | | \\  / |  |" << endl;
-		cout << "  |    / /\\ \\   | |\\/| | | . ` | |  __|    \\___ \\    | |   | |  | | | |\\/| |  |" << endl;
-		cout << "  |   / ____ \\  | |  | | | |\\  | | |____   ____) |  _| |_  | |__| | | |  | |  |" << endl;
-		cout << "  |  /_/    \\_\\ |_|  |_| |_| \\_| |______| |_____/  |_____|  \\____/  |_|  |_|  |" << endl;
-		cout << "  |___________________________________________________________________________|"<<endl;
-		cout << "   \\                                                                         /"<<endl;
-		cout << "    \\             Bartlomiej Ciolkosz All Rights Reserverd 2017.            /"<<endl;
-		cout << "     \\_____________________________________________________________________/"<<endl<<endl<<endl;
-		cout << "                     |   Nacisnij S, aby zaczac gre   |"<<endl;
-		cout << "                     |   Nacisnij Q, aby wyjsc z gry  |"<<endl<<endl;
 		
+		// LVL 1 //
 		
-		wybor = getch();
-			
-		switch (wybor)
-		{
-			case 's':
-			break;
-				case 'q':
-					exit(0);
-			break;
-		    default: cout<< "nie ma takiej opcji";
-		}
-			
-		cout << "Budzisz sie w zamknietym pomieszczeniu na lozku, twoja glowa bardzo cie boli, a obraz poczatkowo wydaje"
-			"sie byc jakby przez mgle. Chwila lezenia pomaga w koncu dojsc do siebie. Pokoj nie jest duzy dostrzegasz jedynie "
+		cout << endl <<">------------------------------{ AMNESIUM LVL1 }-------------------------------<"<<endl<<endl;	
+		cout << " Budzisz sie w zamknietym pomieszczeniu na lozku, twoja glowa bardzo cie boli,\n a obraz poczatkowo wydaje"
+			" sie byc jakby przez mgle. Chwila lezenia pomaga \n w koncu dojsc do siebie. Pokoj nie jest duzy \n dostrzegasz jedynie "
 			"biurko, szafe, obraz oraz drzwi";
 			
-		P1:	
 		cout<<endl<<endl;	
-		cout << "Drzwi <1>" << endl;
-		cout << "Stary Obraz <2>" << endl;
-		cout << "Biurko <3>" << endl;
-		cout << "Szafa <4>" <<endl;
-		cout << "Instrukcja gry <I>";
-				
+		cout << " Drzwi <1>" << endl;
+		cout << " Stary Obraz <2>" << endl;
+		cout << " Biurko <3>" << endl;
+		cout << " Szafa <4>" <<endl;
+		cout << " Instrukcja gry <I>";
+		
 				cout<<endl<<endl;
-				cout << "Wybierz rzecz do ktorej chcesz podejsc :"<< endl << endl;
-
-		wybor = getch();
-			
+				cout << " Wybierz rzecz do ktorej chcesz podejsc :"<< endl << endl;
+		
+		wybor=getch();	
+		
 		switch (wybor)
 		{
 			case '1':
-			cout << "> Podchodzisz do drzwi"; // PRZYPADEK DRZWI//
+				cout << " > Podchodzisz do drzwi"; // PRZYPADEK DRZWI 1 //		
+				cout<<endl<<endl;
 				
-				DR:
+				wybor = getch();
+				switch (wybor)			
+			{	
+							
+				case 'a':
+					
+				cout << " > Bardzo stare drzwi wydaja sie byc zamkniete";
+				break;
+				
+				case 's':
+					if (skey==1)
+				{
+				cout << " > Uzyto klucza drzwi sie otwieraja, czy chcesz przejsc do LVL2 (Y/N) ?";
 				
 				cout<<endl<<endl;
 				
 				wybor = getch();
 				switch (wybor)			
-		{	
+			{	
 							
-				case 'a':
-					
-				cout << " > Bardzo stare drzwi wydaja sie byc zamkniete";
-				goto DR;
+				case 'y':
+				cout << " > Idziesz do LVL2";
 				break;
 				
-				case 's':
-					if (key==1)
-				{
-				cout << " > Uzyto klucza drzwi sie otwieraja";
+				case 'n':
+				cout<< " > Odchodzisz od drzwi  ";
+				break;
+					
+				
+				default: cout<< "";	
+			}		
+				
 				} else
 				cout<< " > Nie mozna otworzyc drzwi";
-				goto DR;
 				break;
 				
 				case 'd':
 				cout << " > Nie ma tutaj nic do zebrania";
-				goto DR;
 				break;
 				
 				case 'w':
-				cout << "> Odchodzisz od przedmiotu";
-				goto P1;
-				break;
+				cout << " > Odchodzisz od przedmiotu";
+				break;	
 				
-				default: cout<< "nie ma takiej opcji";	
-					
-		}		
+				default: cout<< "";	
+			}		
 				
-			break;
-			case '2':
-			cout << "> Podchodzisz do starego obrazu"; // PRZYPADEK OBRAZU //
-					
-				OB:
+		
+					case '2':
+						cout << " > Podchodzisz do starego obrazu"; // PRZYPADEK OBRAZU 1 //
 						
-				cout<<endl<<endl;
-					
-				wybor = getch();
+				cout<<endl;
 				switch (wybor)
-		{
+			{
 				case 'a':
-					
-				cout << " Bardzo stare drzwi wydaja sie byc zamkniete";
-				goto OB;
+				cout << " > Obraz wydaje sie tak jakby dziwnie na ciebie spogladac, ale ciezko powiedziec czy to tylko"
+				" wymysl twojej wyobrazni czy rzeczywiscie tak jest";
 				break;
 				
 				case 's':
-					if (key==1)
-				{
-				cout << " > Uzyto klucza drzwi sie otwieraja";
-				} else
-				cout<< " > Nie mozna otworzyc drzwi";
-				goto OB;
+				cout<< " > Dotykasz ramki obrazu z ktorego wypada cos na podloge zawinietego w zgnity material";
 				break;
 				
 				case 'd':
-				cout << " > Nie ma tutaj nic do zebrania";
-				goto OB;
+				cout << " > Znajdujesz zardzewialy klucz";
+				rkey = 1;
 				break;
 				
 				case 'w':
-				cout << "> Odchodzisz od przedmiotu";
-				goto P1;
-				break;
-				
-				default: cout<< "nie ma takiej opcji";		
-		}
-			break;	
-				case '3':
-				cout << "> Podchodzisz do biurka"; // PRZYPADEK BIURKA //
-						
-				BI:	
-				
+				cout << " > Odchodzisz od przedmiotu";
+				system ("cls");
+				break;		
+			}
+			
+					case '3':
+						cout << " > Podchodzisz do biurka"; // PRZYPADEK BIURKA 1 //
+					
 				cout<<endl<<endl;
 						
 				wybor = getch();
 				switch (wybor)
-		{
+			{
 				case 'a':
 					
-				cout << " > Przeszukujac biurko odnajdujesz mnostwo kurzu oraz klucz";
-				goto BI;
+				cout << " > Cale pognite i paskudne biurko, pasuje do tego mrocznego wystroju.";
 				break;
 				
 				case 's':
-								
-				cout<< " > Przedmiotu biurko nie da sie uzyc";
-				goto BI;
+												
+				cout<< " > Przeszukujac biurko odnajdujesz mnostwo kurzu oraz klucz";
 				break;
 				
 				case 'd':
 				cout << " > Wzieto srebrny klucz";
-				key = 1;
-				goto BI;
+				skey = 1;
 				break;
 				
 				case 'w':
 				cout << "> Odchodzisz od przedmiotu";
-				goto P1;
-				break;
+				break;	
 				
-				default: cout<< "nie ma takiej opcji";		
-		}
-			break;
-				case '4':
-				cout << "> Podchodzisz do szafy"; // PRZYPADEK SZAFY //
-				
+				default: cout<< "";	
+				}	
+		
+						case '4':
+							cout << "> Podchodzisz do szafy"; // PRZYPADEK SZAFY 1 //
+							
 				SZ:
-					
+							
 				cout<<endl<<endl;
 							
 				wybor = getch();
 				switch (wybor)
-		{
+			{
 				case 'a':
 					
-				cout << " Bardzo stare drzwi wydaja sie byc zamkniete";
+				cout << " Szafa ma wyrwane drzwi i jest w tragicznym stanie."
+				"Okazuje sie jednak ze w podlodze ukryty jest schowek zamkniety jakims kluczem";
 				goto SZ;
 				break;
 				
 				case 's':
-					if (key==1)
+				if (rkey==1)
 				{
-				cout << " > Uzyto klucza drzwi sie otwieraja";
+				cout << " > Uzyto klucza, schowek sie otwiera i znajdujesz na jego dnie cos ostrego";
 				} else
-				cout<< " > Nie mozna otworzyc drzwi";
+				cout<< " > Nie mozna otworzyc schowka";
 				goto SZ;
 				break;
 				
 				case 'd':
-				cout << " > Nie ma tutaj nic do zebrania";
+				cout << " > Wzieto stary pordzewialy noz";
+				oknf = 1;
 				goto SZ;
 				break;
 				
-				case 'w':
+			 	case 'w':
 				cout << "> Odchodzisz od przedmiotu";
 				goto P1;
-				break;
 				
-				default: cout<< "nie ma takiej opcji";
-						
-		}
-			break;
+				default: cout<< "";	
+			}
+		
 			
 		case 'i':
 					cout << endl << endl <<endl;
@@ -226,15 +196,17 @@ using namespace std;
 					cout << "                       |   A   | |   S   | |   D   |"<<endl;
 					cout << "                       |_______| |_______| |_______|"<<endl;
 					cout << "                         /           |           \\" <<endl;
-					cout << "                        /    Uzycie przedmiotu    \\" <<endl;
+					cout << "                        /  Uzyj/przeszukaj rzecz  \\" <<endl;
 					cout << "                       /                         Wez przedmiot " << endl;
-					cout << "          Zbadaj przedmiot" << endl << endl << endl << endl;
-					goto P1;		
+					cout << "      Inspekcja przedmiotu" << endl << endl << endl << endl;		
 			break;	
 			
-		    default: cout<< "nie ma takiej opcji";
+		    default : break;
+		
 		}
-
-
+		system("cls");
+		continue;
+		}
+	
 		return (0);
 	}
